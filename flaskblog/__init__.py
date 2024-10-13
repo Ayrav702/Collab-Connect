4ba52df9bed78b6a59894abcb1abc3c9f3fccad7
Ayrav702 <rajaajay20027@gmail.com>
2024-10-13 18:51:24 +0530

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,29 +1,37 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_wtf.csrf import CSRFProtect
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'ZWHluTnEpzKGvfOtTExTGQ'
-csrf_token = 'bQLWTRgM4V1mClWW7YXiGw'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://default:dbXTNQ8CRg4l@ep-jolly-sea-a46263e4-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'
+# Use environment variables for sensitive configurations
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
+app.config['CSRF_TOKEN'] = os.getenv('CSRF_TOKEN', 'default_csrf_token')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+# Mail Configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_TLS'] =  False
+app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'flaskone111@gmail.com'
-app.config['MAIL_PASSWORD'] = 'nwggvoexiynehemh'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
+# User Invitation Configuration
 app.config['USER_ENABLE_INVITE_USER'] = True
 app.config['USER_REQUIRE_INVITATION'] = False
+
 mail = Mail(app)
+csrf = CSRFProtect(app)
 
 from flaskblog import routes
-
